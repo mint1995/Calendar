9b61f3a22315284d6ad5bcac956b07cb89798fde
poramain keawpan <poramain.k@ku.th>
2017-08-28 19:19:09 +0700

    start project

---
+++ src/test/java/ku/cs/calendar/CalendarTest.java
@@ -0,0 +1,14 @@
+package ku.cs.calendar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalendarTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
