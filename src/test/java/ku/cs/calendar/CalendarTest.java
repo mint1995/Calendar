c566c49750ac84ebb173634758faab51acca016d
poramain keawpan <poramain.k@ku.th>
2017-09-06 23:04:59 +0700

    add test case

--- src/test/java/ku/cs/calendar/CalendarTest.java
+++ src/test/java/ku/cs/calendar/CalendarTest.java
@@ -2,6 +2,7 @@ package ku.cs.calendar;
 //Poramain Keawpan 5710404446 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -18,9 +19,34 @@ public class CalendarTest {
 	}
 	
 	@Test
-	public void addDataTest(){
-		
+	public void addDataTest() throws SQLException{
+		DBConnecter db = new DBConnecter();
+		db.connect();
+		db.addData("01/01/2017", "01.00", "test1");
+		db.connect();
+		db.addData("01/01/2017", "01.00", "test2");
+		db.connect();
+		Assert.assertEquals(Script.changeArrayToList(db.getData())[0], "01/01/2017 : 01.00 : test1");
 	}
 	
-
+	@Test
+	//check if delete obj.1 and still have obj.2
+	public void delete2DataTest() throws SQLException{
+		DBConnecter db = new DBConnecter();
+		db.connect();
+		db.delete("01/01/2017", "01.00", "test1");
+		db.connect();
+		Assert.assertEquals(Script.changeArrayToList(db.getData())[0], "01/01/2017 : 01.00 : test2");
+	}
+	
+	@Test
+	//check if delete all obj.
+	public void deleteDataTest() throws SQLException{
+		DBConnecter db = new DBConnecter();
+		db.connect();
+		db.delete("01/01/2017", "01.00", "test2");
+		db.connect();
+		Assert.assertTrue(db.getData().isEmpty());
+	}
+	
 }
